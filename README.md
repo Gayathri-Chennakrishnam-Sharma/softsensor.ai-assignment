582d3129a6de0f1b9ea5dfb92b03435f7a598b10
Gayathri-Chennakrishnam-Sharma <42883702+Gayathri-Chennakrishnam-Sharma@users.noreply.github.com>
2021-11-17 16:09:04 +0530

    Adding Nodejs - shopping cart.
    Adding Nodejs - shopping cart.

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# softsensor.ai-assignment
+# SoftSensor.AI - assignment
+
+### Run locally on your machine
+
+You need to have installed Node and NPM.
+
+![NodeJS Shopping Cart](/data/nodejs-cart-1.png?raw=true "NodeJS Shopping Cart")
+
+Clone this repository
+
+```shell
+https://github.com/Gayathri-Chennakrishnam-Sharma/softsensor.ai-assignment
+```
+
+Install dependencies
+
+```shell
+npm install
+```
+
+Start NodeJS server at http://localhost:3000
+
+```shell
+npm start
+```
+
+For Nodemon usage
+
+```shell
+nodemon ./bin/www
+```
+
+### Technologies
+
+NodeJS, Express, Express-Session, Nodemon, hbs - Express.js view engine for handlebars.js
---
+++ models/cart.js
@@ -0,0 +1,31 @@
+module.exports = function Cart(cart) {
+    this.items = cart.items || {};
+    this.title = cart.title || '';
+    this.totalItems = cart.totalItems || 0;
+    this.totalPrice = Math.round(cart.totalPrice) || 0;
+
+    this.add = function(item, id) {
+        var cartItem = this.items[id];
+        if (!cartItem) {
+            cartItem = this.items[id] = {item: item, quantity: 0, price: 0};
+        }
+        cartItem.quantity++;
+        cartItem.price = cartItem.item.price * cartItem.quantity;
+        this.totalItems++;
+        this.totalPrice += cartItem.item.price;
+    };
+
+    this.remove = function(id) {
+        this.totalItems -= this.items[id].quantity;
+        this.totalPrice -= this.items[id].price;
+        delete this.items[id];
+    };
+    
+    this.getItems = function() {
+        var arr = [];
+        for (var id in this.items) {
+            arr.push(this.items[id]);
+        }
+        return arr;
+    };
+};
